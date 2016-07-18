1ef7f1aca7cfe54f91729e99916a496653b4d109
Miina Sikk <miina.sikk@gmail.com>
2016-07-18 11:35:32 +0300

    Add select handling.

--- js/customize-image-gallery-control.js
+++ js/customize-image-gallery-control.js
@@ -35,12 +35,15 @@
         ready: function() {
             var control = this;
             // Shortcut so that we don't have to use _.bind every time we add a callback.
-            _.bindAll( control, 'openFrame' );
+            _.bindAll( control, 'openFrame', 'select' );
 
             // Bind events.
             control.container.on( 'click keydown', '.upload-button', control.openFrame );
         },
 
+        /**
+         * Open the media modal.
+         */
         openFrame: function() {
             event.preventDefault();
 
@@ -51,6 +54,10 @@
             this.frame.open();
         },
 
+        /**
+         * Initiate the media modal select frame.
+         * Save it for using later in case needed.
+         */
         initFrame: function() {
 
             this.frame = wp.media({
@@ -65,7 +72,47 @@
                     })
                 ]
             });
-        }
+
+            // When files are selected, run a callback.
+            this.frame.on( 'select', this.select );
+        },
+
+        /**
+         * Callback for selecting attachments.
+         */
+        select: function() {
+            var attachments = this.frame.state().get( 'selection' ).toJSON();
+            this.params.attachments = attachments;
+
+            var attachmentIds = this.getAttachmentIds( attachments );
+            console.log( attachmentIds );
+            // Set the Customizer setting; the callback takes care of rendering.
+            this.setSettingValues( attachmentIds );
+        },
+
+        /**
+         * Get array of attachment id-s from attachment objects.
+         *
+         * @param {Array} attachments Attachments.
+         * @returns {Array}
+         */
+        getAttachmentIds: function( attachments ) {
+            var ids = [];
+            for ( var i in attachments ) {
+                ids.push( attachments[i].id );
+            }
+            return ids;
+        },
+
+        /**
+         * Set setting values.
+         *
+         * @param {Array} values Array of ids.
+         */
+        setSettingValues: function( values ) {
+            var control = this;
+            control.setting.set( values );
+        },
     });
 
     api.controlConstructor['image_gallery'] = api.ImageGalleryControl;
